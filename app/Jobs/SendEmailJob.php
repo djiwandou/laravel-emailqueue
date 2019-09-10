957b36eb8edb3a76f0cc92d5199b78c34f1863b7
Djiwandou Putro <djiwandou@tokodistributor.com>
2019-09-10 10:19:53 +0700

    implement SendEmailJob queue

---
+++ app/Jobs/SendEmailJob.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SendMailable;
+
+class SendEmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function retryUntil()
+    {
+        return now()->addSeconds(5);
+    }
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+        Mail::to('admin2@emailjob.com')->send(new SendMailable());
+    }
+}
